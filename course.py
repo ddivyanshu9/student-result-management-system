02ab60eb53f2f7487592d7973b548b4112bf5b51
raputin9 <111039578+raputin9@users.noreply.github.com>
2022-12-12 12:33:43 +0530

    Update course.py

--- course.py
+++ course.py
@@ -1,49 +1,192 @@
 from tkinter import*
 from PIL import Image,ImageTk #pip install pillow
-from course import CourseClass
-class RMS:
+from tkinter import ttk,messagebox
+import sqlite3
+class CourseClass:
     def __init__(self,root):
         self.root = root
         self.root.title("Student Result Management System")
-        self.root.geometry('1350x700+0+0')
+        self.root.geometry('1200x480+80+170')
         self.root.config(bg = "white")
-        #logo
-        # self.logo_dash = Image.open("")
-        title = Label(self.root,text = "Student Result Management System",font =("goudy old style",20,"bold"),bg = "#033054",fg="white").place(x=0,y=0,relwidth=1,height =50)
-        #---Menu----
-        M_Frame =LabelFrame(self.root,text="Menu",font =("times new roman",15),bg ="white")
-        M_Frame.place(x=10,y=70,width=1340,height=80)
-        btn_course=Button(M_Frame,text="Course",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2",command=self.add_course).place(x=20,y=5,width=200,height=40)
-        btn_student=Button(M_Frame,text="Student",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=240,y=5,width=200,height=40)
-        btn_result=Button(M_Frame,text="Result",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=460,y=5,width=200,height=40)
-        btn_view=Button(M_Frame,text="View Student Result",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=680,y=5,width=200,height=40)
-        btn_logout=Button(M_Frame,text="Logout",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=900,y=5,width=200,height=40)
-        btn_exit=Button(M_Frame,text="Exit",font=("goudy old style",15,"bold"),bg="#0b5377",fg="white",cursor="hand2").place(x=1120,y=5,width=200,height=40)
-        #=====content window=======
-        self.bg_img = Image.open("Images/studyingimage.jpg")
-        self.bg_img = self.bg_img.resize((920,350),Image.ANTIALIAS)
-        self.bg_img=ImageTk.PhotoImage(self.bg_img)
+        self.root.focus_force()
+        #=====title=====
+        title = Label(self.root,text = "Manage course details",font =("goudy old style",20,"bold"),bg = "#033054",fg="white").place(x=10,y=15,width=1180,height =35)
+        #======Variable====
+        self.var_course = StringVar()
+        self.var_duration = StringVar()
+        self.var_charges = StringVar()
 
-        self.lbl_bg=Label(self.root,image=self.bg_img).place(x=400,y=180,width=920,height=350)
+        # #=======Widgets====
+        lbl_courseName=Label(self.root,text = "Course Name",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=60)
+        lbl_duration=Label(self.root,text = "Duration",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=100)
+        lbl_charges=Label(self.root,text = "Charges",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=140)
+        lbl_description=Label(self.root,text = "Description",font=("goudy old style",15,"bold"),bg="white").place(x=10,y=180)
 
-        #====update details===
-        self.lbl_course =Label(self.root,text="Total Course\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#e43b06",fg="white")
-        self.lbl_course.place(x=400,y=530,width=300,height=100)
+        #======Entry Fields======
+        self.txt_courseName=Entry(self.root,textvariable=self.var_course,font=("goudy old style",15,"bold"),bg="lightyellow")
+        self.txt_courseName.place(x=150,y=60,width=200)
+        txt_duration=Entry(self.root,textvariable=self.var_duration,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=150,y=100,width=200)
+        txt_charges=Entry(self.root,textvariable=self.var_charges,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=150,y=140,width=200)
+        self.txt_description=Text(self.root,font=("goudy old style",15,"bold"),bg="lightyellow")
+        self.txt_description.place(x=150,y=180,width=500,height=130)
+        #====buttons====
+        self.btn_add=Button(self.root,text="Save",font=("goudy old style",15,"bold"),bg="#2196f3",fg="white",cursor="hand2",command=self.add)
+        self.btn_add.place(x=150,y=400,width=110,height=40)
+        self.btn_update=Button(self.root,text="Update",font=("goudy old style",15,"bold"),bg="#4caf50",fg="white",cursor="hand2",command=self.update)
+        self.btn_update.place(x=270,y=400,width=110,height=40)
+        self.btn_delete=Button(self.root,text="Delete",font=("goudy old style",15,"bold"),bg="#f44336",fg="white",cursor="hand2",command=self.delete)
+        self.btn_delete.place(x=390,y=400,width=110,height=40)
+        self.btn_clear=Button(self.root,text="Clear",font=("goudy old style",15,"bold"),bg="#607d8b",fg="white",cursor="hand2",command=self.clear)
+        self.btn_clear.place(x=510,y=400,width=110,height=40)
 
-        self.lbl_student =Label(self.root,text="Total Student\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#0676ad",fg="white")
-        self.lbl_student.place(x=710,y=530,width=300,height=100)
+        #====Search Panel====
+        self.var_search = StringVar()
+        lbl_search_courseName=Label(self.root,text = "Course Name:",font=("goudy old style",15,"bold"),bg="white").place(x=720,y=60)
+        txt_search_courseName=Entry(self.root,textvariable=self.var_search,font=("goudy old style",15,"bold"),bg="lightyellow").place(x=870,y=60,width=180)
+        btn_search=Button(self.root,text="Search",font=("goudy old style",15,"bold"),bg="#03a9f4",fg="white",cursor="hand2",command=self.search).place(x=1070,y=60,width=120,height=28)
+        #=====content======
+        self.C_Frame =Frame(self.root,bd=2,relief=RIDGE)
+        self.C_Frame.place(x=720,y=100,width=470,height=340)
 
-        self.lbl_Result =Label(self.root,text="Total Results\n[ 0 ]",font=("goudy old style",20),bd=10,relief=RIDGE,bg="#038074",fg="white")
-        self.lbl_Result.place(x=1020,y=530,width=300,height=100)
+        scrolly=Scrollbar(self.C_Frame,orient=VERTICAL)
+        scrollx=Scrollbar(self.C_Frame,orient=HORIZONTAL)
+        self.CourseTable= ttk.Treeview(self.C_Frame,columns=("cid","name","duration","charges","description"),xscrollcommand=scrollx.set,yscrollcommand=scrolly.set)
+        #====scroll=======
+        scrollx.pack(side=BOTTOM,fill=X)
+        scrolly.pack(side=BOTTOM,fill=Y)
+        scrollx.config(command=self.CourseTable.xview)
+        scrolly.config(command=self.CourseTable.yview)
 
-        #====Footer====
-        footer = Label(self.root,text = "SRMS-Student Result Management System made by group f",font =("goudy old style",12),bg = "#262626",fg="white").pack(side =BOTTOM,fill=X)
+        self.CourseTable.heading("cid",text="Course Id")
+        self.CourseTable.heading("name",text="name")
+        self.CourseTable.heading("duration",text="Duration")
+        self.CourseTable.heading("charges",text="Charges")
+        self.CourseTable.heading("description",text="Description")
+        self.CourseTable["show"] = 'headings'
+        self.CourseTable.column("cid",width=100)
+        self.CourseTable.column("name",width=100)
+        self.CourseTable.column("duration",width=100)
+        self.CourseTable.column("charges",width=100)
+        self.CourseTable.column("description",width=150)
+        self.CourseTable.pack(fill=BOTH,expand=1)
+        self.CourseTable.bind("<ButtonRelease-1>",self.get_data)
+        self.show()
+#=========================================================
 
-    def add_course(self):
-        self.new_win=Toplevel(self.root)
-        self.new_obj=CourseClass(self.new_win)
+    def clear(self):
+        self.show()
+        self.var_course.set("")
+        self.var_duration.set("")
+        self.var_charges.set("")
+        self.var_search.set("")
+        self.txt_description.delete('1.0',END)
+        self.txt_courseName.config(state=NORMAL)
+
+    def delete(self):
+        con = sqlite3.connect(database="rms.db")
+        cur = con.cursor();
+        try:
+            if self.var_course.get()=="":
+                messagebox.showerror("Error","Course Name should be required",parent=self.root)
+            else :
+                cur.execute("select * from course where name=?",(self.var_course.get(),))
+                row=cur.fetchone()
+                if row == None:
+                    messagebox.showerror("Error","please select course from the list first",parent=self.root)
+                else:
+                    op=messagebox.askyesno("Confirm","Do you really want to delete",parent = self.root)
+                    if op==True:
+                        cur.execute("delete from course where name =?",(self.var_course.get(),))
+                        con.commit()
+                        messagebox.showinfo("Delete","Course Deleted Successfully",parent=self.root)
+                        self.clear()
+        except Exception as ex:
+          messagebox.showerror("Error",f"Error due to {str(ex)}")
+
+    def get_data(self,ev):
+        self.txt_courseName.config(state='readonly')
+        r= self.CourseTable.focus()
+        content = self.CourseTable.item(r)
+        row = content["values"]
+        self.var_course.set(row[1])
+        self.var_duration.set(row[2])
+        self.var_charges.set(row[3])
+        self.txt_description.delete('1.0',END)
+        self.txt_description.insert(END,row[4])
+
+    def add(self):
+        con=sqlite3.connect(database="rms.db")
+        cur = con.cursor();
+        try:
+            if self.var_course.get()=="":
+                messagebox.showerror("Error","Course Name should be required",parent=self.root)
+            else :
+                cur.execute("select * from course where name=?",(self.var_course.get(),))
+                row=cur.fetchone()
+                if row != None:
+                    messagebox.showerror("Error","Course Name already present",parent=self.root)
+                else:
+                    cur.execute("insert into course (name,duration,charges,description) values(?,?,?,?)",(
+                        self.var_course.get(),
+                        self.var_duration.get(),
+                        self.var_charges.get(),
+                        self.txt_description.get("1.0",END)
+                    ))
+                    con.commit()
+                    messagebox.showinfo("Success","Course Added Successfully",parent = self.root)
+                    self.show()
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to {str(ex)}")
+#=====update=====
+    def update(self):
+        con=sqlite3.connect(database="rms.db")
+        cur = con.cursor();
+        try:
+            if self.var_course.get()=="":
+                messagebox.showerror("Error","Course Name should be required",parent=self.root)
+            else :
+                cur.execute("select * from course where name=?",(self.var_course.get(),))
+                row=cur.fetchone()
+                if row == None:
+                    messagebox.showerror("Error","Select Course from list",parent=self.root)
+                else:
+                    cur.execute("update course set duration=?,charges=?,description=? where name=?",(
+                        self.var_duration.get(),
+                        self.var_charges.get(),
+                        self.txt_description.get("1.0",END),
+                        self.var_course.get()
+                    ))
+                    con.commit()
+                    messagebox.showinfo("Success","Course Update Successfully",parent = self.root)
+                    self.show()
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to {str(ex)}")
+        #===========
+    def show(self):
+        con=sqlite3.connect(database="rms.db")
+        cur = con.cursor();
+        try:
+            cur.execute("select * from course")
+            rows=cur.fetchall()
+            self.CourseTable.delete(*self.CourseTable.get_children())
+            for row in rows:
+                self.CourseTable.insert('',END,values=row)
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to {str(ex)}")
+
+    def search(self):
+        con=sqlite3.connect(database="rms.db")
+        cur = con.cursor();
+        try:
+            cur.execute(f"select * from course where name LIKE '%{self.var_search.get()}%'")
+            rows=cur.fetchall()
+            self.CourseTable.delete(*self.CourseTable.get_children())
+            for row in rows:
+                self.CourseTable.insert('',END,values=row)
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to {str(ex)}")
 
 if __name__=="__main__":
     root =Tk()
-    obj=RMS(root)
+    obj=CourseClass(root)
     root.mainloop()
